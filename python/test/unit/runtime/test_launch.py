6ac18b4057ff6432cca6d09ca227302ec28bd4ae
yrl <2535184404@qq.com>
2026-06-04 19:00:51 +0800

    fix test

--- python/test/unit/runtime/test_launch.py
+++ python/test/unit/runtime/test_launch.py
@@ -158,7 +158,13 @@ def test_launch_with_options(options) -> None:
         if is_cuda():
             libdir = current_dir.parent.parent.parent.parent / 'third_party/nvidia/backend/lib'
             options["extern_libs"] = {"libdevice": str(libdir / 'libdevice.10.bc')}
-            if (os.environ.get("USE_FLAGCX", True)):
+            use_flagcx = os.environ.get("USE_FLAGCX", False)
+            try:
+                import triton.experimental.tle.language as tle
+                use_flagcx = tle.communication.enabled
+            except ImportError:
+                use_flagcx = False
+            if (use_flagcx):
                 lib_flagcx_dir = pathlib.Path.home() / ".flagtree" / "flagcx"
                 options["extern_libs"].update({"libflagcx": str(lib_flagcx_dir / 'libflagcx_device.bc')})
 
